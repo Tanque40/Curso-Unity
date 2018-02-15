1f1bae6d71196ac8d0aa6e3634824805d1d589ce
Bruno <tanque_250@hotmail.com>
2018-02-14 19:57:10 -0600

    Animaciones hacia ambos lados con untity

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour {
+public class Player : MonoBehaviour {
+    //Definimos las cosas a usar
+    Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		
+        //Obtenemos el uso, en este caso un animador
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //Entradas de teclado
+        if (Input.GetKeyDown(KeyCode.D)) {
+            //Indicamos que es lo que vamos a cambiar
+            animator.SetBool("isWalk", true);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.D)){
+            animator.SetBool("isWalk", false);
+        }
+        if (Input.GetKeyDown(KeyCode.A)){
+            //Indicamos que es loq ue vamos a cambiar
+            animator.SetBool("isWalk", true);
+            //Indicamos que al ir a otro lado se va a girar
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.A)){
+            animator.SetBool("isWalk", false);
+        }
+    }
 }
